3f17f1c429c34838c7f676c5bc3982677430b944
Matthew Daggitt <UNIWA\00112652@dep71351vm.uniwa.uwa.edu.au>
2024-04-26 11:31:53 +0800

    Added the ability to actually sign-up groups

--- app/models.py
+++ app/models.py
@@ -7,10 +7,12 @@ class Student(db.Model):
     name = db.Column(db.String(100), nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey('group.group_id'), nullable=True)
 
+    group = db.relationship('Group', back_populates='students')
+
     def __repr__(self) -> str:
         return f'<Student {self.name} {self.uwa_id}>'
-    
+    group_id
 class Group(db.Model):
     group_id = db.Column(db.Integer, primary_key=True)
 
-    students = db.relationship(Student)
+    students = db.relationship(Student, back_populates='group')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import redirect, render_template, request, url_for
-from app import flaskApp
+from flask import flash, redirect, render_template, request, url_for
+from app import flaskApp, db
+from app.forms import CreateGroupForm
 from app.models import Group, Student
 
 @flaskApp.route("/")
@@ -11,12 +12,51 @@ def groups():
 
 @flaskApp.route('/create')
 def create():
-    return render_template('createGroup.html')
+    form = CreateGroupForm()
+    return render_template('createGroup.html', form=form)
     
 @flaskApp.route('/submit', methods=['post'])
 def submit():
-    print(request.method)
-    print(request.form)
-    print(request.form['numberOfStudents'])
-    print("Submitted!")
+    form = CreateGroupForm()
+    if not form.validate_on_submit():
+        return render_template('createGroup.html', form=form)
+    
+    groupSize = int(form.groupSize.data)
+    studentIDs = [
+        form.student1.data,
+        form.student2.data,
+        form.student3.data,
+        form.student4.data        
+    ][:groupSize]
+
+    students = [find_student_by_id(id) for id in studentIDs]
+    if not all(students):
+        return render_template('createGroup.html', form=form)
+    
+    all_unique = len(set(students)) == len(students)
+    if not all_unique:
+        flash("Groups cannot contain duplicate students!")
+        return render_template('createGroup.html', form=form)
+
+    all_free = True
+    for student in students:
+        if student.group:
+            flash(f"{student.name} ({student.uwa_id}) is already assigned to a group")
+            all_free = False
+
+    if not all_free:
+        return render_template('createGroup.html', form=form)
+
+    group = Group()
+    group.students = students
+
+    db.session.add(group)
+    db.session.commit()
+    
     return redirect(location=url_for("groups"))
+
+def find_student_by_id(id:str):
+    student = Student.query.get(id)
+    if not student:
+        flash(f'Could not find student with UWA ID: {id}', 'error')
+    return student
